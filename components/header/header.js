a7cf56836bba6af4430046bca79ccf13c1fa8c7c
nahuoo <nahuel.sigismondi@gmail.com>
2021-03-09 01:19:50 -0300

    primer push

---
+++ components/header/header.js
@@ -0,0 +1,14 @@
+import { MitadNegra, MitadImagen, Div } from './styledHeader'
+
+export const Header = () => {
+    return(
+        <Div>
+            <MitadNegra>
+            <h1>TR</h1>
+            </MitadNegra>
+            <MitadImagen>
+            <h1>IP</h1>
+            </MitadImagen>
+        </Div>   
+    )
+}
---
+++ components/header/styledHeader.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Div = styled.div`
+    display: flex;    
+`
+
+export const MitadNegra = styled.div`
+    width: 50vw;
+    height: 100vh;
+    background: black;
+    color:white;
+    display:flex;
+    justify-content: flex-end;
+    align-items:center;
+    
+`
+
+export const MitadImagen = styled.div`
+    width: 50vw;
+    height: 100vh;
+    background: white;
+    color:black;
+    display:flex;
+    justify-content: flex-start;
+    align-items:center;
+`
